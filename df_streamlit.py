29a2b59c8bd64db79679f58f4c6778a1f9804967
tuukka <seppatuukka@gmail.com>
2026-05-23 12:33:01 +0300

    streamlit mods

--- df_streamlit.py
+++ df_streamlit.py
@@ -26,7 +26,7 @@ if lit:
     st.set_page_config(layout = "wide")
 df = pd.read_csv("df_for_streamlit.csv")
 df = df.drop(columns = ['is_sauna', 'is_balcony','is_elevator'])
-print(df.columns)
+#print(df.columns)
 
 df['streetname_and_number'] = df['street'].astype(str) + " " + df['street_number'].astype(str)
 #df = df.drop(columns = ['cond_adequate', 'cond_terrible', 'cond_unclassified'])
@@ -35,24 +35,31 @@ styled_df = df.style.background_gradient(subset=['hinta'], cmap='RdYlGn_r')
 
 #Filter that returns the correct dataframe based on checkboxes
 status = []
-def dataframe_selector(df, active = True, passive = True):
-    allowed = []
+def dataframe_selector(df, active = True, passive = True, kv = True, rk = True, mo = True, hp = True, kmm = True):
+    allowed_active = []
+    allowed_districts = []
     if active:
-        allowed.append(True)
+        allowed_active.append(True)
     if passive:
-        allowed.append(False)
-    return df[df['active'].isin(allowed)]
+        allowed_active.append(False)
+
+    if kv:
+        allowed_districts.append('kaukovainio')
+    if rk:
+        allowed_districts.append('rajakylä')
+    if mo:
+        allowed_districts.append('myllyoja')
+    if hp:
+        allowed_districts.append('heinapaa')
+    if kmm:
+        allowed_districts.append('kmm')
+
+    mask = df['active'].isin(allowed_active) & df['district'].isin(allowed_districts)
+    return df[mask]
 
 
 aktiiviset = df[df['active'] == True]
 poistuneet = df[df['active'] == False]
-
-#yksio = df[df['rooms'] == 1]
-#kaksio = df[df['rooms'] == 2]
-##kolmio = df[df['rooms'] == 3]
-#nelio = df[df['rooms'] == 4]
-#muut_koot = df[df['rooms'] > 4]
-#koottomat = df[df['rooms'] == 0]
 
 kadun_mukaan = df.groupby(['street']).agg(
     halvin =('price','min'),
@@ -60,25 +67,32 @@ kadun_mukaan = df.groupby(['street']).agg(
 )
 
 if lit:
+    st.sidebar.subheader("Number of rooms")
     yksio_checkbox = st.sidebar.checkbox(label = "1h", value=True)
     kaksio_checkbox = st.sidebar.checkbox(label = "2h", value=True)
     kolmio_checkbox = st.sidebar.checkbox(label = "3h", value=True)
     nelio_checkbox = st.sidebar.checkbox(label = "4h", value=True)
     muut_checkbox = st.sidebar.checkbox(label = "4+h", value=True)
+    st.sidebar.subheader("Active / Passive")
     active_checkbox = st.sidebar.checkbox(label = "Active listings", value = True)
     passive_checkbox = st.sidebar.checkbox(label = "Passive listings", value = True)
+    st.sidebar.subheader("District")
+    kaukovainio_checkbox = st.sidebar.checkbox(label = "kaukovainio", value = True)
+    rajakyla_checkbox = st.sidebar.checkbox(label = "rajakyla", value = True) 
+    myllyoja_checkbox = st.sidebar.checkbox(label = "myllyoja", value = True) 
+    heinapaa_checkbox = st.sidebar.checkbox(label = "heinapaa", value = True) 
+    kmm_checkbox = st.sidebar.checkbox(label = "Kaakkuri, Metelinkangas", value = True) 
 
-    df_rows = df.shape[0]
-    df_columns = df.shape[1]
+    #df_rows = df.shape[0]
+    #df_columns = df.shape[1]
 
-    df = dataframe_selector(df, active=active_checkbox, passive=passive_checkbox)
-
-
+    df = dataframe_selector(df, active=active_checkbox, passive=passive_checkbox, kv = kaukovainio_checkbox, rk = rajakyla_checkbox, mo = myllyoja_checkbox, hp = heinapaa_checkbox, kmm = kmm_checkbox)
+    
     tab1, tab2, tab3, tab4 = st.tabs(["Grouped by room count", "Colored", "Boxplot", "Grouped by street"])
 
     with tab1:
         #st.write(f"Rows {df_rows}, columns {df_columns}")
-        st.write(df.shape)
+        #st.write(df.shape)
         if yksio_checkbox:
             st.write("One room")
             yksio = df[df['rooms'] == 1]
